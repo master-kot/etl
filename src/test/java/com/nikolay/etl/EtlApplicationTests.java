bbe30ff25048cb761aaea18bd6a33d499c6ac836
master-kot <master-kot@mail.ru>
2023-05-27 17:53:37 +0300

    Base of service has created

---
+++ src/test/java/com/nikolay/etl/EtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolay.etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
